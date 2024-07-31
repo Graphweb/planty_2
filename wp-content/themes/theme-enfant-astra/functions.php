1fc280854d0e842616f0c0c499d3951f8a3acb84
PARIEL Dominique <dominiquepariel88@gmail.com>
2024-07-31 18:28:35 +0400

    creation theme.css

--- wp-content/themes/theme-enfant-astra/functions.php
+++ wp-content/themes/theme-enfant-astra/functions.php
@@ -4,6 +4,8 @@
 // **/
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
+    // lien pour appeller le css du theme parent
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+//  lien pour appeller le css du theme enfant
+wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
