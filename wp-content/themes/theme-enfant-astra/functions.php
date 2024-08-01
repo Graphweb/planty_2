21525400eb436b274e8d4de502e093ce27e77e73
PARIEL Dominique <dominiquepariel88@gmail.com>
2024-08-01 22:44:39 +0400

    création des pages + hook

--- wp-content/themes/theme-enfant-astra/functions.php
+++ wp-content/themes/theme-enfant-astra/functions.php
@@ -9,3 +9,11 @@ function theme_enqueue_styles() {
 //  lien pour appeller le css du theme enfant
 wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+// code pour ciblé avec un hook//
+function ajouter_lien_admin($items, $args) {
+    if (is_user_logged_in() && $args->theme_location == 'primary') {
+        $items .= '<li class="menu-item"><a href="' . admin_url() . '">Admin</a></li>';
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin', 10, 2);
